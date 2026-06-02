4df74afb1285a9e1e3646cebdd634d824f473791
ibrahimh61 <ibrahimh61@nycstudents.net>
2026-06-02 17:20:52 +0000

    gbdkigbdl

--- WDLesson7_3aCW_Charts/script.js
+++ WDLesson7_3aCW_Charts/script.js
@@ -30,32 +30,34 @@ async function init(){
 }
 
 // Arrays of information
-let math = ["Math",79.0,80.0,79.9,78.8,73.4,77.2,74.5,78.7,76.8 ];
-let english = ["English",79.7,78.8,81.6,80.6,82.3,77.4,79.5,78.6,77.9];
-let science = ["Science",78.0,81.5,81.6,81.6,82.2,82.4,81.3,80.1,78.2];
-let social = ["Social Studies",95.0,80.6,80.8,81.5,81.8,80.6,81.1,80.9,80.6];
+// shows the statics of the victims from Dec 2025 - may 2026
+let Bronx = ["Bronx",12, 14, 15, 11, 13, 10 ];
+let Manhattan  = ["Manhattan ",7, 6, 5, 8, 7, 6];
+let Brooklyn  = ["Brooklyn",14, 13, 12, 17, 16, 18];
+let Queens  = ["Queens",3, 3, 2, 4, 5, 5];
+let StatenIsland  = ["Staten Island",0, 0, 1, 1, 1, 4];
 
 
 function chart(type){
   //Task 3: Combine the math, english, science and social data into one array and store in a variable 'data'
-  data = [      ];
+  data = [Bronx, Manhattan, Brooklyn, Queens, StatenIsland];
   
   //Task 4: Using the "type" of chart passed, call the function below to create an appropriate chart of that type in the "chart" container using the data from Task 3
-  displayChart(       );
+  displayChart(data, "chart", type);
 }
 
 //Task 5: Create a array "budget" with at least 6 items. Recall that budget must be structured as an array of arrays.
 let budget =[
-  ["Rent",1500],
-  ["Food",600],
-  ["Car",500],
-  
-
+  ["Bronx",75],
+  ["Manhattan",39],
+  ["Brooklyn",90],
+  ["Queens",22],
+  ["Staten Island",7],
 ];
 
 function budgetChart(){
   //Task 6: Display the budget as a pie chart
-  displayChart(      );
+  displayChart(budget, "chart", "pie");
 }
 
 
